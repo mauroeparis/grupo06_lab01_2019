060b303ec4e3da3764bf78aff92bba28972fc3de
Julian Merida <julianmr97@gmail.com>
2019-03-29 12:41:20 -0300

    hacer: funcion cambiar, sem y arreglar: mapDib
    Co-Authored-By: Mauro Ezequiel Paris <mauroparis96@gmail.com>
    Co-Authored-By: JuliBorre <borrejulieta@gmail.com>

--- Dibujo.hs
+++ Dibujo.hs
@@ -61,11 +61,32 @@ bas x = Basica x
 
 -- map para nuestro lenguaje
 mapDib :: (a -> b) -> Dibujo a -> Dibujo b
-mapDib (d1 -> Dibujo d1) d2 = d2
-mapDib fun Basica d1 = fun d1
-mapDib fun Rotar d1 = Rotar mapDib fun d1
-mapDib fun Rot45 d1 = Rot45 mapDib fun d1
-mapDib fun Espejar d1 = Espejar mapDib fun d1
-mapDib fun Apilar i1 i2 d1 d2 = Apilar i1 i2 mapDib fun d1 mapDib fun d2
-mapDib fun Juntar i1 i2 d1 d2 = Juntar i1 i2 mapDib fun d1 mapDib fun d2
-mapDib fun Encimar d1 d2 = Encimar mapDib fun d1 mapDib fun d2
+mapDib fun (Basica d1) = Basica (fun d1)
+mapDib fun (Rotar d1) = Rotar (mapDib fun d1)
+mapDib fun (Rot45 d1) = Rot45 (mapDib fun d1)
+mapDib fun (Espejar d1) = Espejar (mapDib fun d1)
+mapDib fun (Apilar i1 i2 d1 d2) = Apilar i1 i2 (mapDib fun d1) (mapDib fun d2)
+mapDib fun (Juntar i1 i2 d1 d2) = Juntar i1 i2 (mapDib fun d1) (mapDib fun d2)
+mapDib fun (Encimar d1 d2) = Encimar (mapDib fun d1) (mapDib fun d2)
+
+cambiar :: (a -> Dibujo b) -> Dibujo a -> Dibujo b
+cambiar fun (Basica d1) = fun d1
+cambiar fun (Rotar d1) = Rotar (cambiar fun d1)
+cambiar fun (Rot45 d1) = Rot45 (cambiar fun d1)
+cambiar fun (Espejar d1) = Espejar (cambiar fun d1)
+cambiar fun (Apilar i1 i2 d1 d2) = Apilar i1 i2 (cambiar fun d1) (cambiar fun d2)
+cambiar fun (Juntar i1 i2 d1 d2) = Juntar i1 i2 (cambiar fun d1) (cambiar fun d2)
+cambiar fun (Encimar d1 d2) = Encimar (cambiar fun d1) (cambiar fun d2)
+
+sem :: (a -> b) -> (b -> b) -> (b -> b) -> (b -> b) ->
+       (Int -> Int -> b -> b -> b) ->
+       (Int -> Int -> b -> b -> b) ->
+       (b -> b -> b) ->
+       Dibujo a -> b
+sem bas rot esp rot45 api juntar encimar (Basica d) = bas d
+sem bas rot esp rot45 api juntar encimar (Rotar d) = rot (sem bas rot esp rot45 api juntar encimar d)
+sem bas rot esp rot45 api juntar encimar (Rot45 d) = esp (sem bas rot esp rot45 api juntar encimar d)
+sem bas rot esp rot45 api juntar encimar (Espejar d) = rot45 (sem bas rot esp rot45 api juntar encimar d)
+sem bas rot esp rot45 api juntar encimar (Apilar i1 i2 d1 d2) = api i1 i2 (sem bas rot esp rot45 api juntar encimar d1) (sem bas rot esp rot45 api juntar encimar d2)
+sem bas rot esp rot45 api juntar encimar (Juntar i1 i2 d1 d2) = juntar i1 i2 (sem bas rot esp rot45 api juntar encimar  d1) (sem bas rot esp rot45 api juntar encimar d2)
+sem bas rot esp rot45 api juntar encimar (Encimar d1 d2) = encimar (sem bas rot esp rot45 api juntar encimar d1) (sem bas rot esp rot45 api juntar encimar d2)
