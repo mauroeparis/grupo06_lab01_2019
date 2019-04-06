f3499b39635e597c0abb9b77dd631e0300a06c24
Mauro Ezequiel Paris <mauroparis96@gmail.com>
2019-04-06 18:31:21 -0300

    escher.hs movido a la carpeta Basico

--- Basico/Ejemplo.hs
+++ Basico/Ejemplo.hs
@@ -3,9 +3,15 @@ import Dibujo
 import Interp
 
 data Base = Triangulo1
+            | Triangulo2
+            | TrianguloD
+            | Rectangulo
 
 ejemplo :: Dibujo Base
 ejemplo = Basica Triangulo1
 
 interpBas :: Output Base
 interpBas Triangulo1 = trian1
+interpBas Triangulo2 = trian2
+interpBas TrianguloD = trianD
+interpBas Rectangulo = rectan
